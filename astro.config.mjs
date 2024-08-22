3c48a79af87d2d0e0350e9e7563d098db66eea4c
Leonardo Dominguez <odranoel.leonardo98@gmail.com>
2024-08-22 15:06:36 -0300

    distable config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,16 +6,16 @@ import yaml from '@rollup/plugin-yaml';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  vite: {
-    plugins: [yaml()]
-  },
+  // vite: {
+  //   plugins: [yaml()]
+  // },
   output: 'hybrid',
-  adapter: cloudflare({
-    imageService: 'cloudflare',
-  }),
-  build: {
-    client: './client'
-  },
+  // adapter: cloudflare({
+  //   imageService: 'cloudflare',
+  // }),
+  // build: {
+  //   client: './client'
+  // },
   integrations: [
     react(), 
     tailwind({
