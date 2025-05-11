ccca04dcb67e5a202fce6c3a909daac9e44521b6
Odranoel998 <Odranoel.leonardo98@gmail.com>
2025-05-11 11:52:32 -0300

    Cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "https://odranoel998.github.io",
+  base: "https://odranoel998.github.io/PortafolioAstro/",
   integrations: [tailwind()]
 });
